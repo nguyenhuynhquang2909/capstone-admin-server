ea8146d7679e678f0b5679b7b5bf81ef0134c3c0
KhangPynamo <khangtgr@gmail.com>
2024-04-22 08:36:11 +0000

    update-20240422-3:36PM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ServerController } from './server/server.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({ isGlobal: true }),
+    DatabaseModule,
+  ],
+  controllers: [ServerController],
   providers: [],
 })
 export class AppModule {}
