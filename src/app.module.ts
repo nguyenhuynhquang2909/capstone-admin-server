aa609ddf677dcff250f7b5657be5d0aab2824eeb
KhangPynamo <khangtgr@gmail.com>
2024-04-22 10:20:01 +0000

    update-20240422-5:20PM
    Description: adding CRUD operations for 'user' entity, updating database schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ServerController } from './server/server.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CacheModule.register({ isGlobal: true }),
     DatabaseModule,
+    UserModule,
   ],
   controllers: [ServerController],
   providers: [],
