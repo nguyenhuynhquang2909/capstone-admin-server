abebbb750056a9f9053494949e67a7765e7b6691
KhangLeonardo <s3929994@rmit.edu.vn>
2024-04-23 05:09:07 +0000

    update-20240423-12:09PM
    Description: updating '/verify' to store accessToken to database, refactoring '/logout'

--- schema/init.sql
+++ schema/init.sql
@@ -12,10 +12,8 @@ CREATE TABLE users (
 CREATE TABLE user_sessions (
     id SERIAL PRIMARY KEY,
     access_token VARCHAR(255) NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL,
     user_id INTEGER REFERENCES users(id),
     access_token_expiration_time TIMESTAMPTZ NOT NULL,
-    refresh_token_expiration_time TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
@@ -26,9 +24,3 @@ INSERT INTO users (name, phone, last_login) VALUES
 ('Jane Smith', '0987654321', '2024-04-19 15:30:00'),
 ('Alice Brown', '1122334455', NULL),
 ('Bob White', '5544332211', '2024-04-20 10:45:00');
-
--- Dummy data for user_sessions table
--- INSERT INTO user_sessions (access_token, refresh_token, user_id, access_token_expiration_time, refresh_token_expiration_time) VALUES
--- ('token123', 'refresh123', 1, '2024-05-20 12:00:00', '2024-05-21 12:00:00'),
--- ('token456', 'refresh456', 2, '2024-05-21 15:30:00', '2024-05-22 15:30:00'),
--- ('token789', 'refresh789', 4, '2024-05-22 10:45:00', '2024-05-23 10:45:00');
