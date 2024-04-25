1d3d39dd08f0ec26aeda22d52b3bcb942f299e43
KhangLeonardo <s3929994@rmit.edu.vn>
2024-04-25 08:01:58 +0000

    Adding JWT Strategy to fix the jwt not found error
    update-20240425-03:02PM

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,18 @@ import { User } from '../user/entities/user.entity';
 import { UserSession } from './entities/user-session.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, UserSession]),
     JwtModule.register({
       secret: '123456',
-      signOptions: { expiresIn: '13608000s' },
+      signOptions: { expiresIn: '10368000s' },
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
