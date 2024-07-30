df2434d25b60fdd61e3fb9d47e39a196ba192674
KhangLeonardo <s3929994@rmit.edu.vn>
2024-07-30 04:27:27 +0000

    add create & edit posts /post route
    without SDK connecting to S3

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { AuthModule } from './apis/auth/auth.module';
 
 // Configs
 import { PostgresModule } from './configs/postgres.module';
+import { PostModule } from './apis/post/post.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PostgresModule,
     AuthModule,
+    PostModule,
   ],
 })
 export class AppModule {}
