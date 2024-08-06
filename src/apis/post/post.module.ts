eb08d0aca9f817bb049fd33db993f37271ad0ec9
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-08-06 11:25:15 +0700

    Update create post

--- src/apis/post/post.module.ts
+++ src/apis/post/post.module.ts
@@ -5,10 +5,12 @@ import { PostService } from './post.service';
 import { Post } from '../../common/entities/post.entity';
 import { SchoolAdmin } from '../../common/entities/school-admin.entity';
 import { JwtService } from '../../common/jwt/jwt.service';
+import { Media } from 'src/common/entities/media.entity';
+import { MediaService } from '../media/media.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, SchoolAdmin])],
+  imports: [TypeOrmModule.forFeature([Post, SchoolAdmin, Media])],
   controllers: [PostController],
-  providers: [PostService, JwtService],
+  providers: [PostService, JwtService, MediaService],
 })
 export class PostModule {}
