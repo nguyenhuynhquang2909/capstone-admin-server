5340f1434dde859b08d7e3358a7007e10f8d3256
KhangLeonardo <s3929994@rmit.edu.vn>
2024-08-22 17:07:21 +0000

    fix module error

--- src/apis/student/student.module.ts
+++ src/apis/student/student.module.ts
@@ -7,7 +7,9 @@ import { JwtService } from 'src/common/jwt/jwt.service';
 import { SchoolAdmin } from 'src/common/entities/school-admin.entity';
 import { User } from 'src/common/entities/user.entity';
 import { Media } from 'src/common/entities/media.entity';
-import { StudentMedia } from 'src/common/entities/student-media.entity';
+import { PostMedia } from '../../common/entities/post-media.entity';
+import { StudentMedia } from '../../common/entities/student-media.entity';
+import { MealMedia } from '../../common/entities/meal-media.entity';
 import { MediaService } from '../media/media.service';
 import { Class } from 'src/common/entities/class.entity';
 import { UserModule } from '../user/user.module';
@@ -20,7 +22,9 @@ import { UserModule } from '../user/user.module';
       User,
       Media,
       StudentMedia,
-      Class
+      Class,
+      PostMedia,
+      MealMedia
     ]),
     UserModule
   ],
